59a5be3861d94f615a0893814ca7e82fe1a0f474
Ebugo <gospelokpara@gmail.com>
2020-04-09 08:18:06 +0100

    worked on the first 3 COVID19 build for SDGs challenges

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,28 @@
-const covid19ImpactEstimator = (data) => data;
+/* eslint-disable linebreak-style */
+const impact = require('./impact');
+const severeImpact = require('./severeImpact');
 
+const covid19ImpactEstimator = (data) => {
+  // console.log(impact);
+  // console.log(severeImpact);
+  return {
+    data,
+    impact,
+    severeImpact
+  };
+};
+
+// covid19ImpactEstimator({
+//   region: {
+//     name: "Africa",
+//     avgAge: 19.7,
+//     avgDailyIncomeInUSD: 5,
+//     avgDailyIncomePopulation: 0.71
+//   },
+//   periodType: "days",
+//   timeToElapse: 58,
+//   reportedCases: 674,
+//   population: 66622705,
+//   totalHospitalBeds: 1380614
+// });
 export default covid19ImpactEstimator;
---
+++ src/impact.js
@@ -0,0 +1,28 @@
+/* eslint-disable linebreak-style */
+const percentage = (num, percent) => Math.floor((num / 100) * percent);
+
+const impact = (data) => {
+  const currentlyInfected = data.reportedCases * 10;
+  const infectionsByRequestedTime = currentlyInfected * (2 ** 10);
+
+  const severeCasesByRequestedTime = percentage(currentlyInfected, 15);
+  const availableHospitalBeds = percentage(data.totalHospitalBeds, 35);
+  const hospitalBedsByRequestedTime = availableHospitalBeds - severeCasesByRequestedTime;
+
+  const casesForICUByRequestedTime = percentage(infectionsByRequestedTime, 5);
+  const casesForVentilatorsByRequestedTime = percentage(infectionsByRequestedTime, 2);
+  const dollarsInFlight = (infectionsByRequestedTime * data.avgDailyIncomePopulation) * data.avgDailyIncomeInUSD * 30;
+
+  return {
+    currentlyInfected,
+    infectionsByRequestedTime,
+    severeCasesByRequestedTime,
+    availableHospitalBeds,
+    hospitalBedsByRequestedTime,
+    casesForICUByRequestedTime,
+    casesForVentilatorsByRequestedTime,
+    dollarsInFlight
+  };
+};
+
+export default impact;
