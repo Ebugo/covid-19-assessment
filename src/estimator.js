2c85ac57c4de0b35b3187669ee50e372df89e146
Ebugo <gospelokpara@gmail.com>
2020-04-09 13:15:07 +0100

    round dollars in flight

--- src/estimator.js
+++ src/estimator.js
@@ -38,7 +38,7 @@ const impact = (data) => {
     hospitalBedsByRequestedTime,
     casesForICUByRequestedTime,
     casesForVentilatorsByRequestedTime,
-    dollarsInFlight: dollarsInFlight.toFixed(1)
+    dollarsInFlight: Math.floor(dollarsInFlight)
   };
 };
 
@@ -78,7 +78,7 @@ const severeImpact = (data) => {
     hospitalBedsByRequestedTime,
     casesForICUByRequestedTime,
     casesForVentilatorsByRequestedTime,
-    dollarsInFlight: dollarsInFlight.toFixed(1)
+    dollarsInFlight: Math.floor(dollarsInFlight)
   };
 };
 
