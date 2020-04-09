ff31898842f761e30bb6de00d56dea3171b1a3c1
Ebugo <gospelokpara@gmail.com>
2020-04-09 10:58:48 +0100

    time conversion

--- src/estimator.js
+++ src/estimator.js
@@ -3,25 +3,25 @@ let numOfDays;
 let factor;
 
 const impact = (data) => {
-  const percentage = (num, percent) => Math.floor((num / 100) * percent);
+  const percentage = (num, percent) => Math.round((num / 100) * percent);
   const avgIncomePopulation = data.region.avgDailyIncomePopulation;
   const avgIncome = data.region.avgDailyIncomeInUSD;
 
   if (data.periodType === 'days') {
     numOfDays = data.timeToElapse;
-    factor = Math.floor(numOfDays / 3);
+    factor = Math.round(numOfDays / 3);
   } else if (data.periodType === 'weeks') {
     numOfDays = data.timeToElapse * 7;
-    factor = Math.floor(numOfDays / 3);
+    factor = Math.round(numOfDays / 3);
   } else if (data.periodType === 'months') {
     numOfDays = data.timeToElapse * 28;
-    factor = Math.floor(numOfDays / 3);
+    factor = Math.round(numOfDays / 3);
   } else {
     return 'Invalid data type';
   }
 
   const currentlyInfected = data.reportedCases * 10;
-  const infectionsByRequestedTime = currentlyInfected * (2 ** factor) / 2;
+  const infectionsByRequestedTime = currentlyInfected * (2 ** factor);
 
   const severeCasesByRequestedTime = percentage(currentlyInfected, 15);
   const availableHospitalBeds = percentage(data.totalHospitalBeds, 35);
@@ -44,25 +44,25 @@ const impact = (data) => {
 };
 
 const severeImpact = (data) => {
-  const percentage = (num, percent) => Math.floor((num / 100) * percent);
+  const percentage = (num, percent) => Math.round((num / 100) * percent);
   const avgIncomePopulation = data.region.avgDailyIncomePopulation;
   const avgIncome = data.region.avgDailyIncomeInUSD;
 
   if (data.periodType === 'days') {
     numOfDays = data.timeToElapse;
-    factor = Math.floor(numOfDays / 3);
+    factor = Math.round(numOfDays / 3);
   } else if (data.periodType === 'weeks') {
     numOfDays = data.timeToElapse * 7;
-    factor = Math.floor(numOfDays / 3);
+    factor = Math.round(numOfDays / 3);
   } else if (data.periodType === 'months') {
     numOfDays = data.timeToElapse * 28;
-    factor = Math.floor(numOfDays / 3);
+    factor = Math.round(numOfDays / 3);
   } else {
     return 'Invalid data type';
   }
 
   const currentlyInfected = data.reportedCases * 50;
-  const infectionsByRequestedTime = currentlyInfected * (2 ** factor) / 2;
+  const infectionsByRequestedTime = currentlyInfected * (2 ** factor);
 
   const severeCasesByRequestedTime = percentage(currentlyInfected, 15);
   const availableHospitalBeds = percentage(data.totalHospitalBeds, 35);
