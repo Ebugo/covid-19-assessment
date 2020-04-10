b97e16d4667e33a4f3fa30abac783d1698e3080e
Ebugo <gospelokpara@gmail.com>
2020-04-10 13:01:46 +0100

    swap multipliers

--- src/estimator.js
+++ src/estimator.js
@@ -27,8 +27,8 @@ const impact = (data) => {
   const availableHospitalBeds = percentage(data.totalHospitalBeds, 35);
   const hospitalBedsByRequestedTime = availableHospitalBeds - severeCasesByRequestedTime;
 
-  const casesForICUByRequestedTime = percentage(infectionsByRequestedTime, 2);
-  const casesForVentilatorsByRequestedTime = percentage(infectionsByRequestedTime, 5);
+  const casesForICUByRequestedTime = percentage(infectionsByRequestedTime, 3);
+  const casesForVentilatorsByRequestedTime = percentage(infectionsByRequestedTime, 2);
   const dollarsInFlight = (infectionsByRequestedTime * avgIncomePopulation) * avgIncome * numOfDays;
 
   return {
