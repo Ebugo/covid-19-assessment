5ce1ecc296b75e0a9338feaedd63909c734e1e38
Ebugo <gospelokpara@gmail.com>
2020-04-10 12:31:43 +0100

    truncated all output

--- src/estimator.js
+++ src/estimator.js
@@ -27,8 +27,8 @@ const impact = (data) => {
   const availableHospitalBeds = percentage(data.totalHospitalBeds, 35);
   const hospitalBedsByRequestedTime = availableHospitalBeds - severeCasesByRequestedTime;
 
-  const casesForICUByRequestedTime = percentage(infectionsByRequestedTime, 5);
-  const casesForVentilatorsByRequestedTime = percentage(infectionsByRequestedTime, 2);
+  const casesForICUByRequestedTime = percentage(Math.trunc(infectionsByRequestedTime), 5);
+  const casesForVentilatorsByRequestedTime = percentage(Math.trunc(infectionsByRequestedTime), 2);
   const dollarsInFlight = (infectionsByRequestedTime * avgIncomePopulation) * avgIncome * numOfDays;
 
   return {
