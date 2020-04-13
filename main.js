38e186cb18800e9d14d94edb05b72df118d6e5fb
Ebugo <gospelokpara@gmail.com>
2020-04-13 05:05:27 +0100

    fixed css issue

--- main.js
+++ main.js
@@ -114,8 +114,8 @@ const onViewEstimate = () => {
   const result = covid19ImpactEstimator(infoData);
   if (isNaN(result.impact.dollarsInFlight)) {
     resultDiv.innerHTML = `
-      <span>No estimate yet.</span><br>
-      <span>Input correct values please.</span>
+      <span>No estimate yet.</span><br><br>
+      <span style="color:red">Input correct values please.</span>
     `
   } else {
     resultDiv.innerHTML = `
